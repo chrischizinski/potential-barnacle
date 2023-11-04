93e80fbb0e2cd791ba1c53969cc5f458c516ecee
Christopher J Chizinski <cchizinski2@unl.edu>
2023-11-03 21:14:15 -0500

    changed bib creation to RefManageR

--- functions.r
+++ functions.r
@@ -6,13 +6,16 @@ get_current_year <- function(){
         year()
 }
 
+authors = "D S Kane and K L Pope and K D Koupal and M A Pegg and C J Chizinski and M A Kaemingk"
+title = "The influence of the COVI"
+
 create_bitex_key <- function(authors, title, year){
   # requires stringr and tm packages
 
-  first_author_name <- str_extract(authors, pattern = "\\s([[:alpha:]]+),")
-  first_author_name <- str_remove(first_author_name, pattern = "[[:punct:]]")
+  first_author_name <- str_extract(authors, pattern = "\\S+\\s+(\\S+)\\s+\\S+")
+  first_author_name <- str_remove(first_author_name, pattern = "^(\\S+\\s+){2}")
 
-  title_words <- str_remove_all(title, pattern = paste0('\\b', tm::stopwords('english'), collapse = ' |'))
+  title_words <- str_remove_all(tolower(title), pattern = paste0('\\b', tm::stopwords('english'), collapse = ' |'))
   first_title_word <- str_extract(title_words, pattern = "\\b\\w+")
 
   key <- tolower(str_squish(paste0(first_author_name, year, first_title_word)))
--- pull_citations.r
+++ pull_citations.r
@@ -1,8 +1,8 @@
 # Test script to generate a publications list from google scholar for the lab
-
+BibOptions(check.entries = FALSE, bib.style = "authoryear")
 # Load libraries
 require(xfun)
-xfun::pkg_attach2(c("tidyverse", "scholar", "vitae", "tinytex", "bib2df", "tm", "quarto"), message = FALSE)
+xfun::pkg_attach2(c("tidyverse", "scholar", "vitae", "tinytex", "RefManageR", "tm", "quarto"), message = FALSE)
 source("functions.r")
 
 quarto::quarto_add_extension("mps9506/quarto-cv", no_prompt = TRUE)
@@ -10,39 +10,33 @@ quarto::quarto_add_extension("mps9506/quarto-cv", no_prompt = TRUE)
 # set id information 
 
 tribble(
-        ~person, ~scholar_id, ~year_in, ~year_out, ~pubs,
-        "Chris Chizinski",   'kAdpcMUAAAAJ&hl', 2011, NA, NA,
-        "Matt Gruntorad",   'IpqOh28AAAAJ&hl', 2015, NA, NA) -> lab_scholar_ids
+        ~person, ~scholar_id, ~year_in, ~year_out, pubs,
+        "Chris Chizinski",   'kAdpcMUAAAAJ', 2011, NA, NA
+        "Matt Gruntorad",   'IpqOh28AAAAJ', 2015, NA, NA) -> lab_scholar_ids
 
-BibOptions(check.entries = FALSE, bib.style = "authoryear")
-
-test <- ReadGS(scholar.id = "kAdpcMUAAAAJ", limit = Inf, sort.by.date = TRUE)
-as_tibble(test)
 lab_scholar_ids |> 
   mutate(year_out = ifelse(is.na(year_out), get_current_year(), year_out))  ->  lab_scholar_ids2
 
 lab_scholar_ids2 |> 
         group_by(scholar_id) |>
-        nest(data = pubs) |> 
-        rename(pubs = data) |> 
         mutate(pubs = map(.x = scholar_id,
-                        ~scholar::get_publications(.x))) |> 
+                        ~as.data.frame(RefManageR::ReadGS(scholar.id =.x, limit = Inf, sort.by.date = TRUE, check.entries = FALSE))))  |> 
         unnest(pubs) |> 
         ungroup() |> 
-        distinct(pubid, .keep_all = TRUE) |>
+        distinct(title, .keep_all = TRUE) |>
         filter(year >= year_in & year <= year_out)  |> 
-        select(author, title, journal, number, year)  |>
-        mutate(bibtexkey = create_bitex_key(author, title, year),
-        bibtype = "article",
-        number = separate_number_scholar(number))  |> 
-        separate(number, into = c("volume", "issue", "pages"), sep = "\\|")  |>
-        mutate(across(c(volume, issue, pages), ~str_replace(.x, "NA", "")))  |>
-        mutate(author = str_split(author, pattern = ", ")) |>
-        rename_all(toupper)  |> 
-        arrange(desc(YEAR), AUTHOR)->  pubs_with_id
+        select(-person, -scholar_id, -cites, -institution, -type)  |>
+        mutate(bibtexkey = create_bitex_key(author, title, year)) |> 
+        mutate(across(c(volume, number, pages), ~str_replace(.x, "NA", "")))  |>
+        arrange(desc(year), author)  |> 
+        as.data.frame() ->  pubs_with_id
+
+
+as.BibEntry(pubs_with_id) -> pubs_bibentry
 
 dir.create('bib', showWarnings = FALSE)
 
-bib2df::df2bib(pubs_with_id, file = here::here('bib', 'lab_pubs.bib'))
+RefManageR::WriteBib(pubs_bibentry, file = here::here('bib', 'lab_pubs.bib'),
+                      )
 
 quarto::quarto_render("ref-template.qmd")
